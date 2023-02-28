8c2f7ba5c66160b7a8193ee6e5db891fdb25412c
CesarMartinez23 <oliver-cesar@hotmail.com>
2023-02-27 22:29:10 -0600

    Bugs Resolved

--- src/components/Navbar/Navbar.vue
+++ src/components/Navbar/Navbar.vue
@@ -17,29 +17,29 @@ export default {
 </script>
 
 <template>
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top p-2">
-    <a class="navbar-brand text-uppercase" href="#"
-      >Parcial Programaci&oacute;n IV</a
-    >
-    <button
-      class="navbar-toggler"
-      type="button"
-      data-toggle="collapse"
-      data-target="#navbarNav"
-      aria-controls="navbarNav"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-    >
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item active" v-for="route in routes" :key="route">
-          <router-link :to="route.path" class="nav-link">{{
-            route.name
-          }}</router-link>
-        </li>
-      </ul>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="/">Parcial</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item" v-for="route in routes" :key="route">
+            <router-link :to="route.path" class="nav-link">{{
+              route.name
+            }}</router-link>
+          </li>
+        </ul>
+      </div>
     </div>
   </nav>
 </template>
--- src/views/countries/index.vue
+++ src/views/countries/index.vue
@@ -16,11 +16,13 @@ export default {
   <div class="row h-100 w-100">
     <h1>API Countries</h1>
   </div>
+  <br />
   <div class="row h-100 w-100">
     <div class="col-sm-3">
       <filters />
     </div>
     <div class="col-sm-9">
+      <h2 class="text-center">Regions</h2>
       <countries />
     </div>
     <div class="cotainer d-flex align-items-center justify-content-center">
